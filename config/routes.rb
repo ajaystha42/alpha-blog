505dd93704420748f4d45d71a32e20d4c99200bf
Ajay Shrestha <er.ajaystha@gmail.com>
2022-10-07 09:54:26 +0545

    devise route redirection

--- config/routes.rb
+++ config/routes.rb
@@ -3,8 +3,8 @@ Rails.application.routes.draw do
   devise_scope :admin_user do  
     get '/admin_users/sign_out' => 'devise/sessions#destroy'     
  end
-# root 'home#index'
-root 'pages#home'
+root 'home#index'
+# root 'pages#home'
 get 'about', to: 'pages#about'
 get 'contact', to: 'pages#contact'
 get 'careers', to: 'pages#careers'
